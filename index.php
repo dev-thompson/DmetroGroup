1285d75bc98e50151caa59a68d1f72c0f5df8df2
dev-thompson <agwujohnama@gmail.com>
2025-05-15 01:51:49 +0100

    commit for home page

--- index.php
+++ index.php
@@ -3,9 +3,224 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="shortcut icon" href="./assets/image/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/icons/css/all.css">
+    <link rel="stylesheet" href="./assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <title>WealthForge</title>
 </head>
 <body>
-    
+
+    <!-- =============== Navbar section =============== -->
+     <nav class="all-navbar">
+        <div class="navbar-content container">
+            <div class="nav-left">
+                <img width="200" src="./assets/image/black-logo.png" alt="">
+            </div>
+            <div class="nav-right">
+                <a href="">HOME</a>
+                <a href="">ABOUT</a>
+                <a href="">
+                    <button class="main-button">JOIN US</button>
+                </a>
+            </div>
+        </div>
+     </nav>
+    <!-- =============== end of Navbar section =============== -->
+
+
+    <!-- ================ hero section ==================== -->
+     <section class="all-hero pt-5 mt-5">
+        <div class="hero-content margin">
+            <div class="hero-left">
+                <img src="./assets/image/plot.png" alt="">
+            </div>
+            <div class="hero-right">
+                <h1>
+                    <span>SIMPLY</span>
+                    <span> <strong>PROFITABLE</strong></span>
+                    <span>CONVENIENTLY</span>
+                </h1>
+                <p>A Profitable platform for high-margin investment</p>
+                <div class="hero-right-cta">
+                    <button class="main-button">Get Started Now!</button>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- ================ end of hero section ==================== -->
+
+
+    <!-- =================== intro section ======================== -->
+    <section class="all-intro pb-5">
+        <div class="intro-content">
+            <div class="intro-item">
+                <div class="intro-image">
+                    <img src="./assets/image/counter01.png" alt="">
+                </div>
+                <div class="intro-title">
+                    <h1>36.9M</h1>
+                    <p>REGISTERED USERS</p>
+                </div>
+            </div>
+            <div class="intro-item">
+                <div class="intro-image">
+                    <img src="./assets/image/counter02.png" alt="">
+                </div>
+                <div class="intro-title">
+                    <h1>174</h1>
+                    <p>COUNTRIES SUPPORTED</p>
+                </div>
+            </div>
+            <div class="intro-item">
+                <div class="intro-image">
+                    <img src="./assets/image/counter03.png" alt="">
+                </div>
+                <div class="intro-title">
+                    <h1><i class="fa fa-dollar-sign"></i>10.8M</h1>
+                    <p>AVERAGE INVESTMENT</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- =================== end of intro section ======================== -->
+
+    <!-- ==================== about section ============================ -->
+     <section class="all-about py-5">
+        <div class="about-content margin">
+            <div class="about-left">
+                <img src="./assets/image/about.png" alt="">
+            </div>
+            <div class="about-right">
+                <h5>WELCOME TO WealthForge</h5>
+                <h1>ABOUT WEALTHFORGE</h1>
+                <p>WealthForge is an investment company, whose team is working on making money from the volatility of cryptocurrencies and offer great returns to our clients.</p>
+                <div class="about-items mt-5 mb-4">
+                    <div class="about-item-1">
+                        <div class="about-image">
+                            <img src="./assets/image/about01.png" alt="">
+                        </div>
+                        <div class="about-title">
+                            <h4>Secure & Reliable</h4>
+                            <p>Secure assets fund for users</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="about-items mb-4">
+                    <div class="about-item-1">
+                        <div class="about-image">
+                            <img src="./assets/image/about02.png" alt="">
+                        </div>
+                        <div class="about-title">
+                            <h4>Fast Withdrawals</h4>
+                            <p>Quick money withdrawals for users</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="about-items">
+                    <div class="about-item-1">
+                        <div class="about-image">
+                            <img src="./assets/image/about03.png" alt="">
+                        </div>
+                        <div class="about-title">
+                            <h4>Guaranteed</h4>
+                            <p>Your return on investment is guaranteed</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- ==================== end of about section ============================ -->
+
+    <!-- ========================= features section =========================== -->
+     <section class="all-features">
+        <div class="features-content margin">
+            <h4>Our Amazing Features</h4>
+            <h1>WHY SHOULD YOU INVEST</h1>
+            <p>
+                <span>We are worldwide investment company who are committed to the principle of revenue</span> 
+                <span>maximization and reduction of the financial risks at investing.</span>
+            </p>
+            <div class="fetures pt-5">
+                <div class="features-1">
+                    <div class="features-image mb-4">
+                        <img src="./assets/image/feature01.png" alt="">
+                    </div>
+                    <div class="features-title">
+                        <h4>PROFITABLE INVESTMENT</h4>
+                        <p>WealthForge has a very big profitable investment</p>
+                    </div>
+                </div>
+                <div class="features-1">
+                    <div class="features-image mb-4">
+                        <img src="./assets/image/feature02.png" alt="">
+                    </div>
+                    <div class="features-title">
+                        <h4>DDS PROTECTION</h4>
+                        <p>WealthForge has a very high DDS protection</p>
+                    </div>
+                </div>
+                <div class="features-1">
+                    <div class="features-image mb-4">
+                        <img src="./assets/image/feature03.png" alt="">
+                    </div>
+                    <div class="features-title">
+                        <h4>24/7 SUPPORT CENTER</h4>
+                        <p>Here at WealthForge our customer support is 24/7</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- ========================= end of features section =========================== -->
+
+
+    <!-- ========================= get to know section =========================== -->
+     <section class="all-features pt-5">
+        <div class="features-content margin">
+            <h4>Get To Know</h4>
+            <h1>HOW WE WORK?</h1>
+            <p>
+                <span>Follow these simple steps and make profit!</span>
+            </p>
+            <div class="get-to-know container">
+                <div class="know-left">
+                    <img src="./assets/image/how01.png" alt="">
+                </div>
+                <div class="know-right">
+                    <div class="know-right-content mb-3">
+                        <div class="step-number">
+                            <h1>01</h1>
+                        </div>
+                        <div class="step-title">
+                            <h4>INSTANT REGISTRATION</h4>
+                            <P>Click <a href="">Sign Up</a> to fill the blank, our 256 SSL will Protect your privacy.</P>
+                        </div>
+                    </div>
+                    <div class="know-right-content mb-3">
+                        <div class="step-number">
+                            <h1>02</h1>
+                        </div>
+                        <div class="step-title">
+                            <h4>INSTANT REGISTRATION</h4>
+                            <P>Click <a href="">Sign Up</a> to fill the blank, our 256 SSL will Protect your privacy.</P>
+                        </div>
+                    </div>
+                    <div class="know-right-content">
+                        <div class="step-number">
+                            <h1>03</h1>
+                        </div>
+                        <div class="step-title">
+                            <h4>INSTANT REGISTRATION</h4>
+                            <P>Click <a href="">Sign Up</a> to fill the blank, our 256 SSL will Protect your privacy.</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- ========================= end of get to know section =========================== -->
+
 </body>
 </html>
